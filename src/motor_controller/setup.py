a23f4c54922c1ec8e663bd704e96fb0be65c1985
Ryo Isogawa <2023m002@kuas.ac.jp>
2024-11-06 13:36:16 +0900

    motor_controller pkg was added

--- src/motor_controller/setup.py
+++ src/motor_controller/setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='csanimatronics',
-    maintainer_email='csanimatronics@todo.todo',
+    maintainer_email='2023m002@kuas.ac.jp',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
