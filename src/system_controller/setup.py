29821d51810802fd4b6ac49f375c6d52b19398ec
Isogawa Ryo <isogawa.ryo@gmail.com>
2024-11-06 17:19:06 +0900

    edit settings

--- src/system_controller/setup.py
+++ src/system_controller/setup.py
@@ -11,15 +11,19 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools',
+                      'rclpy',
+                      'sensor_msg'],
     zip_safe=True,
-    maintainer='csanimatronics',
+    maintainer='Ryo Isogawa',
     maintainer_email='2023m002@kuas.ac.jp',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='a package for controlling the system',
+    license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'controller_publisher = controller_publisher:main',
+            'system_controller = system_controller.system_controller:main',
         ],
     },
 )
