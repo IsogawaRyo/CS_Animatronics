6a534eb64f32531481acb6f6e81ebd75ad30bd70
Isogawa Ryo <isogawa.ryo@gmail.com>
2024-11-10 14:10:59 +0900

    update package.xml

--- src/motor_controller/setup.py
+++ src/motor_controller/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'dynamixel_sdk'],
     zip_safe=True,
     maintainer='Ryo Isogawa',
     maintainer_email='2023m002@kuas.ac.jp',
