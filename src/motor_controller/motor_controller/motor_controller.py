f01079827520827ab2c2cbde5cce522ac772a1fd
Isogawa Ryo <isogawa.ryo@gmail.com>
2024-11-10 11:15:54 +0900

    pull

--- src/motor_controller/motor_controller/motor_controller.py
+++ src/motor_controller/motor_controller/motor_controller.py
@@ -1,1 +1,28 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2024 Ryo Isogawa 　　　　　
+# SPDX-License-Identifier: BSD-3-Claus
 
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Joy
+from motor_command_msg.msg import IdAngle
+
+class MotorController(Node):
+    def __init__(self):
+        super().__init__('motor_controller')
+
+        # Setting for subscriber
+        self.subscription = self.create_subscription(
+            Joy,
+            'controller_input',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning  
+
+
+        # Setting for publisher
+        self.publisher = self.create_publisher(
+            IdAngle,
+            'output_topic',
+            10
+        )
