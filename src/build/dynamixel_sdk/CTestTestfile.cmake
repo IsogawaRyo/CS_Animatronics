41fcb220713735e58ec4fbd48cbb2383e1c38eff
Ryo Isogawa <2023m002@kuas.ac.jp>
2024-11-11 18:31:11 +0900

    add sdks

--- src/build/dynamixel_sdk/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/csanimatronics/CS_Animatronics/src/dynamixel_sdk
-# Build directory: /home/csanimatronics/CS_Animatronics/src/build/dynamixel_sdk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
