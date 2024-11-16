891c207775e2023c7006139740fdf3f02a498453
Ryo Isogawa <2023m002@kuas.ac.jp>
2024-11-16 18:00:42 +0900

    Update motor_controller.py

--- src/motor_controller/motor_controller/motor_controller.py
+++ src/motor_controller/motor_controller/motor_controller.py
@@ -112,7 +112,7 @@ def main(args=None):
         if dxl_comm_result != COMM_SUCCESS:
             print("Failed to set Position Control Mode")
         else:
-            packet_handler.write1ByteTxRx(port_handler, 1, ADDR_LED, 1)
+            packet_handler.write1ByteTxRx(port_handler, id, ADDR_LED, 1)
             print("Succeeded to set Position Control Mode")
         
         # Enable Torque
