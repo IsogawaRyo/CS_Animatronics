9b46aef8f02239eee0bf2dade8137f4732a53f2a
Ryo Isogawa <2023m002@kuas.ac.jp>
2024-12-26 23:10:36 +0900

    update

--- ros2_start.sh
+++ ros2_start.sh
@@ -6,8 +6,8 @@ source /opt/ros/jazzy/setup.bash
 
 sudo -S usermod -aG dialout csanimatronics <<< "KUASECSA"
 
-gnome-terminal -- bash -c "source ~/CS_Animatronics/install/setup.bash && source ~/CS_Animatronics/install/local_setup.bash && ros2 run system_controller system_controller"
+gnome-terminal -- bash -c "source ~/CS_Animatronics/install/setup.bash && source ~/CS_Animatronics/install/local_setup.bash && source ~/CS_Animatronics/install/setup.bash && ros2 run system_controller system_controller"
 
-gnome-terminal -- bash -c "source ~/CS_Animatronics/install/setup.bash && source ~/CS_Animatronics/install/local_setup.bash && ros2 run controller_publisher controller_publisher"
+gnome-terminal -- bash -c "source ~/CS_Animatronics/install/setup.bash && source ~/CS_Animatronics/install/local_setup.bash && source ~/CS_Animatronics/install/setup.bash  && ros2 run controller_publisher controller_publisher"
 
-gnome-terminal -- bash -c "source ~/CS_Animatronics/install/setup.bash && source ~/CS_Animatronics/install/local_setup.bash && ros2 run motor_controller motor_controller"
+gnome-terminal -- bash -c "source ~/CS_Animatronics/install/setup.bash && source ~/CS_Animatronics/install/local_setup.bash && source ~/CS_Animatronics/install/setup.bash  && sudo -S usermod -aG dialout csanimatronics <<< "KUASECSA" && ros2 run motor_controller motor_controller"
