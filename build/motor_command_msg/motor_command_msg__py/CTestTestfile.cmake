8e673514c82c72519264f9264e7de11818ae2ebe
Ryo Isogawa <2023m002@kuas.ac.jp>
2024-12-31 10:20:38 +0900

    updated

--- build/motor_command_msg/motor_command_msg__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/csanimatronics/CS_Animatronics/build/motor_command_msg/motor_command_msg__py
-# Build directory: /home/csanimatronics/CS_Animatronics/build/motor_command_msg/motor_command_msg__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
