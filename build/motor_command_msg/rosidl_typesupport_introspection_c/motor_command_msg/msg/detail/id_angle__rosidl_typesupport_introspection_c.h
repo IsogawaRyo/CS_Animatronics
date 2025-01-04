68f1fae0fa13bf9b576923aaae85aaad7720ac46
Ryo Isogawa <2023m002@kuas.ac.jp>
2025-01-04 10:28:19 +0900

    save

---
+++ build/motor_command_msg/rosidl_typesupport_introspection_c/motor_command_msg/msg/detail/id_angle__rosidl_typesupport_introspection_c.h
@@ -0,0 +1,26 @@
+// generated from rosidl_typesupport_introspection_c/resource/idl__rosidl_typesupport_introspection_c.h.em
+// with input from motor_command_msg:msg/IdAngle.idl
+// generated code does not contain a copyright notice
+
+#ifndef MOTOR_COMMAND_MSG__MSG__DETAIL__ID_ANGLE__ROSIDL_TYPESUPPORT_INTROSPECTION_C_H_
+#define MOTOR_COMMAND_MSG__MSG__DETAIL__ID_ANGLE__ROSIDL_TYPESUPPORT_INTROSPECTION_C_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+#include "rosidl_runtime_c/message_type_support_struct.h"
+#include "rosidl_typesupport_interface/macros.h"
+#include "motor_command_msg/msg/rosidl_typesupport_introspection_c__visibility_control.h"
+
+ROSIDL_TYPESUPPORT_INTROSPECTION_C_PUBLIC_motor_command_msg
+const rosidl_message_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_introspection_c, motor_command_msg, msg, IdAngle)();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // MOTOR_COMMAND_MSG__MSG__DETAIL__ID_ANGLE__ROSIDL_TYPESUPPORT_INTROSPECTION_C_H_
---
+++ install/controller_publisher/lib/python3.12/site-packages/controller_publisher/controller_publisher.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2024 Ryo Isogawa 　　　　　
+# SPDX-License-Identifier: BSD-3-Claus
+
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Joy
+import pygame
+
+class ControllerPublisher(Node):
+    def __init__(self):
+        super().__init__('controller_publisher')
+        self.get_logger().info('Run controller publisher node')
+        
+        self.publisher_ = self.create_publisher(Joy, 'controller_input', 10)
+        
+        self.timer = self.create_timer(0.05, self.timer_callback)
+        pygame.init()
+        pygame.joystick.init()
+
+        # check connections 
+        if pygame.joystick.get_count() == 0:
+            self.get_logger().error('No joystick detected. Please connect a controller!')
+            exit()
+
+        self.controller = pygame.joystick.Joystick(0)
+        self.controller.init()
+        self.get_logger().info('controller initialized')
+
+    def timer_callback(self):
+        pygame.event.pump()
+        msg = Joy()
+
+        # Joy Axes(RStickx, RSticky, RTriger, LStickx, ,LSticky, LTriger)
+        axes = []
+        for i in range(self.controller.get_numaxes()):
+            axes.append(self.controller.get_axis(i))
+        msg.axes = axes
+
+        # Buttons(circle, cross, square, triangle,...)
+        buttons = []
+        for i in range(self.controller.get_numbuttons()):
+            buttons.append(self.controller.get_button(i))
+        msg.buttons = buttons
+
+        # Publish message
+        self.publisher_.publish(msg)
+        self.get_logger().info('Published controller state')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ControllerPublisher()
+    rclpy.spin(node)
+    
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
