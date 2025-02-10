f62c33cacb068692f39002e4f4086e55a7ebb3cf
Ryo Isogawa <2023m002@kuas.ac.jp>
2025-02-10 17:31:54 +0900

    Update MotionEditor.py

--- MotionEditor.py
+++ MotionEditor.py
@@ -6,7 +6,17 @@ import json
 timeStamp = 0
 
 # load motor limits
-motorLimits = {}
+with open("/home/csanimatronics/CS_Animatronics/Motor_Limits.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+for key, subdict in data.items():
+    subdict["ini"] = int(subdict["ini"])
+    subdict["min"] = int(subdict["min"])
+    subdict["max"] = int(subdict["max"])
+    subdict["acc"] = int(subdict["acc"])
+    subdict["vel"] = int(subdict["vel"])
+
+motorLimits = data
 
 # main loop
 root = Tkinter.Tk()
