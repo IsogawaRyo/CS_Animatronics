b1921c91c6d08b8de764749334669aba31403fb4
Ryo Isogawa <2023m002@kuas.ac.jp>
2025-02-10 18:35:38 +0900

    Update MotionEditor.py

--- MotionEditor.py
+++ MotionEditor.py
@@ -2,25 +2,27 @@ import sys
 import Tkinter
 import json
 
-# time at edit point
-timeStamp = 0
+class MotionEditor:
+    def __init__(self):
+        # time at edit point
+        self.timestamp = 0
+        
+        root = Tkinter.Tk()
+        root.title("Motion Editor")
+        root.geometry("1200x400")
+        
+        root.mainloop()
+        
+    def loadMotorLimits(self):
+        # load motor limits
+        with open("/home/csanimatronics/CS_Animatronics/Motor_Limits.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+            
+        for key, subdict in data.items():
+            subdict["ini"] = int(subdict["ini"])
+            subdict["min"] = int(subdict["min"])
+            subdict["max"] = int(subdict["max"])
+            subdict["acc"] = int(subdict["acc"])
+            subdict["vel"] = int(subdict["vel"])
 
-# load motor limits
-with open("/home/csanimatronics/CS_Animatronics/Motor_Limits.json", "r", encoding="utf-8") as file:
-    data = json.load(file)
-
-for key, subdict in data.items():
-    subdict["ini"] = int(subdict["ini"])
-    subdict["min"] = int(subdict["min"])
-    subdict["max"] = int(subdict["max"])
-    subdict["acc"] = int(subdict["acc"])
-    subdict["vel"] = int(subdict["vel"])
-
-motorLimits = data
-
-# main loop
-root = Tkinter.Tk()
-
-
-
-root.mainloop()
+        motorLimits = data
