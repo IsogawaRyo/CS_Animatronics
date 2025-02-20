1fbb1c9a4f8c9e6d2d0c813be06502015bf3a98b
Isogawa Ryo <isogawa.ryo@gmail.com>
2025-02-20 11:45:07 +0900

    update

--- src/motion_editor/setup.py
+++ src/motion_editor/setup.py
@@ -11,16 +11,19 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools',
+                     'rclpy',
+                     'motor_command_msg',],
     zip_safe=True,
     maintainer='csanimatronics',
     maintainer_email='2023m002@kuas.ac.jp',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='package for motion editor',
+    license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'motion_editor = motion_editor.motion_editor:main'
+            'motion_editor = motion_editor.motion_editor:main',
+            'system_controller = system_controller.system_controller:main',
         ],
     },
 )
