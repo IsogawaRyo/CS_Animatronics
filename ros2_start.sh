c25d307eb52b402395b4e6ce6df272033cecc010
Ryo Isogawa <2023m002@kuas.ac.jp>
2025-02-21 07:49:18 +0900

    Update ros2_start.sh

--- ros2_start.sh
+++ ros2_start.sh
@@ -13,3 +13,5 @@ gnome-terminal -- bash -c "source ~/CS_Animatronics/install/setup.bash && source
 gnome-terminal -- bash -c "source ~/CS_Animatronics/install/setup.bash && source ~/CS_Animatronics/install/local_setup.bash && source ~/CS_Animatronics/install/setup.bash  && ros2 run controller_publisher controller_publisher"
 
 gnome-terminal -- bash -c "source ~/CS_Animatronics/install/setup.bash && source ~/CS_Animatronics/install/local_setup.bash && source ~/CS_Animatronics/install/setup.bash  && sudo -S usermod -aG dialout csanimatronics <<< "KUASECSA" && ros2 run motor_controller motor_controller"
+
+gnome-terminal -- bash -c "source ~/CS_Animatronics/install/setup.bash && source ~/CS_Animatronics/install/local_setup.bash && source ~/CS_Animatronics/install/setup.bash  && ros2 run motion_editor motion_editor"
