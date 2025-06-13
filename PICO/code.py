2b0df7feea61f6fde19b4a654fef7df549491a58
Ryo Isogawa <2023m002@kuas.ac.jp>
2025-06-13 09:49:25 +0900

    Update code.py

--- PICO/code.py
+++ PICO/code.py
@@ -1,34 +1,52 @@
 import time
 import board
 import busio
+import struct
+import usb_cdc
 import adafruit_bno055
 import adafruit_tca9548a
-import usb_cdc
-import struct
 
-# I2C初期化（GP1=SCL, GP0=SDA）
+# I2C 初期化
 i2c = busio.I2C(board.GP1, board.GP0)
-
-# TCA9548A初期化（I2Cマルチプレクサ）
 tca = adafruit_tca9548a.TCA9548A(i2c)
 
-# BNO055を3チャンネルから取得
+# BNO055 センサー初期化（CH0〜CH2）
 sensors = []
 for i in range(3):
     try:
-        sensors.append(adafruit_bno055.BNO055_I2C(tca[i]))
+        sensor  = adafruit_bno055.BNO055_I2C(tca[i])
+        time.sleep(0.2)  # 各センサごとに待機（安定化）
+        sensors.append(sensor)
     except ValueError:
         sensors.append(None)
 
 # データ送信ループ
 while True:
+    packet = bytearray()
+
+    # ヘッダー
+    packet += b'\xAA\x55'
+    packet += struct.pack("<B", 3)  # センサ数
+
+    # 各センサデータ
     for ch, sensor in enumerate(sensors):
         if sensor is None:
-            continue
-        euler = sensor.euler
-        if euler is not None:
-            heading, roll, pitch = euler
-            packet = struct.pack("<Bfff", ch, heading, roll, pitch)
-            usb_cdc.data.write(packet)
-    time.sleep(0.05)
+            # センサがない場合は NaN を送る
+            packet += struct.pack("<Bfff", ch, float('nan'), float('nan'), float('nan'))
+        else:
+            euler = sensor.euler
+            if euler is not None:
+                heading, roll, pitch = euler
+                packet += struct.pack("<Bfff", ch, heading, roll, pitch)
+            else:
+                packet += struct.pack("<Bfff", ch, float('nan'), float('nan'), float('nan'))
+
+    # フッター
+    packet += b'\x55\xAA'
+
+    # USBに送信
+    usb_cdc.data.write(packet)
+
+    # 少し待機（調整可）
+    time.sleep(0.02)
 
